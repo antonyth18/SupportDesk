895171e16d2c01489134a49ba0fe5ea7675d71b2
Antony <antonyth1818@gmail.com>
2026-02-18 03:40:49 +0530

    add full docker compose setup with postgres

---
+++ core/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Starting server..."
+exec "$@"
